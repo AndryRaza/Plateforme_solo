8aee280445c7ccd9669a806471b5caeb4262e6e9
AndryRaza <70998539+AndryRaza@users.noreply.github.com>
2020-12-01 11:19:56 +0400

    Correction alimentation prix

--- scripts/acheter.php
+++ scripts/acheter.php
@@ -5,11 +5,11 @@
 if ($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['acheter']))
 {
     $j = $_POST['id_produit']; //On récupère l'index du produit cliqué
-    $produit = json_decode(file_get_contents('../data/cards.json'),true); //On ouvre le fichier contenant les infos des produits
+    $produit = json_decode(file_get_contents('../data/card.json'),true); //On ouvre le fichier contenant les infos des produits
 
-   (int)$produit[$j]['price'] += (int)$produit[$j]['price_up']*0.01; //On augmente du prix_up de la carte qu'on multiplie par 0.01 car ce sont des centimes
+   $produit[$j]['price'] += $produit[$j]['price_up']*0.01; //On augmente du prix_up de la carte qu'on multiplie par 0.01 car ce sont des centimes
 
-    file_put_contents('../data/data.txt', json_encode($produit)); //On enregistre les modifications 
+    file_put_contents('../data/card.json', json_encode($produit)); //On enregistre les modifications 
 }
 
 
